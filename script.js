d2ab53d98049e98b781c3eee7240ffd0d433feac
apoljac <poljaa02@zedat.fu-berlin.de>
2023-08-30 19:52:33 +0200

    created play function

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function getComputerChoice (items){
+function getComputerChoice(items){
     return items[Math.floor(Math.random()*items.length)];
 }
 
+function play(playerSelection, computerSelection){
+    let p_score = 0;
+    let c_score = 0;
+    
+    if ((playerSelection == "Rock" && computerSelection == "Rock") 
+    || (playerSelection == "Paper" && computerSelection == "Paper")
+    || (playerSelection == "Scissors" && computerSelection == "Scissors")){
+        console.log(`Tie. Both selected |${playerSelection}|.`);
+    }
+    else if ((playerSelection == "Rock" && computerSelection == "Scissors") 
+    || (playerSelection == "Paper" && computerSelection == "Rock")
+    || (playerSelection == "Scissors" && computerSelection == "Paper")){
+        ++p_score;
+        console.log(`You win! You selected |${playerSelection}| and Computer selected |${computerSelection}|.`);
+    }
+    else if ((playerSelection == "Rock" && computerSelection == "Paper") 
+    || (playerSelection == "Paper" && computerSelection == "Scissors")
+    || (playerSelection == "Scissors" && computerSelection == "Rock")){
+        ++c_score;
+        console.log(`You lose! You selected |${playerSelection}| and Computer selected |${computerSelection}|.`);
+    }
+    else {
+        console.log("Something went wrong. Sorry.");
+    }
+
+}
+
 const element = ["Rock", "Paper", "Scissors"];
-console.log(getComputerChoice(element));
+//console.log(getComputerChoice(element));
+
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice(element);
+console.log(play(playerSelection, computerSelection));
